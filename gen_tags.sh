0023cfffff24c9d6f02c34947caca050937ef26c
zhongyongsheng <zhongyongsheng@ipin.com>
2018-04-04 11:07:29 +0800

    添加--langmap=c++:.h,支持头文件c++的相关定义

--- gen_tags.sh
+++ gen_tags.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #gen tags in current dir (./)
-ctags -R -I __THROWNL -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q  -f ~/.vim/systags 
+ctags -R -I __THROWNL -I __THROW -I __attribute_pure__ -I __nonnull -I __attribute__ --file-scope=yes --langmap=c:+.h --langmap=c++:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+iaS --extra=+q  -f ~/.vim/systags 
